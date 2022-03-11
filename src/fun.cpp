722350ca2072f8c56fae05b1d838976641b26d0b
IlMkn <91212388+IlMkn@users.noreply.github.com>
2022-03-11 19:21:13 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -19,7 +19,8 @@ unsigned int faStr1(const char *str) {
         if (str[i] >= 48 && str[i] <= 57) {
             key = 1;
         }
-        if (((str[i] == ' ')&&(prev != ' '))||(str[i] == '\0')&&(prev != ' ')) {
+        if (((str[i] == ' ') && (prev != ' ')) || (str[i] == '\0') 
+            && (prev != ' ')) {
             if (key == 1) {
                 num--;
                 key = 0;
@@ -41,16 +42,18 @@ unsigned int faStr2(const char *str) {
     }
     j++;
     for (int i = 0; i < j; i++) {
-        if ((str[i] != ' ')&&(prev == ' ')&&(str[i] != '\0')) {
+        if ((str[i] != ' ') && (prev == ' ') && (str[i] != '\0')) {
             if ((str[i] <= 64) || (str[i] >= 91)) {
                 key1 = 1;
             }
             num++;
         }
-        if (((str[i] <= 64)||(str[i] >= 123))&&(str[i] != '\0')&&(str[i] != ' ')) {
+        if (((str[i] <= 64) || (str[i] >= 123)) && (str[i] != '\0') 
+            && (str[i] != ' ')) {
             key0 = 1;
         }
-        if (((str[i] == ' ')&&(prev != ' '))||(str[i] == '\0')&&(prev != ' ')) {
+        if (((str[i] == ' ') && (prev != ' ')) || (str[i] == '\0') 
+            && (prev != ' ')) {
             if ((key0 == 1) || (key1 == 1)) {
                 num--;
                 key0 = 0;
