269b8333342752ebb8845ebfca21524f2a473935
IlMkn <91212388+IlMkn@users.noreply.github.com>
2022-03-11 19:30:52 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 #include "fun.h"
 
 int main() {
-    const char* str1 = "      qe 1e 2 eqweqw 23132qweqweqwe eew2    q2  rrr   3   r      ";
-    const char* str2 = "      Qe 1e 2 Eqweqw 23132qweqweqwe Eew2    q2  rRr   3   r      Rr2 ";
+    const char* str1 = "      qe 1e 2 eqweqw 23132qweqweqwe eew2  
+        q2  rrr   3   r      ";
+    const char* str2 = "      Qe 1e 2 Eqweqw 23132qweqweqwe Eew2   
+        q2  rRr   3   r      Rr2 ";
     const char* str3 = " ewtyg qwertf     ";
     std::cout << "faStr1 - " << faStr1(str1);
     std::cout << "\n";
